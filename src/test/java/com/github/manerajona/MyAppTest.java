77f03efca26cfa03f8cce9c2fe348ba6fc92ff4f
Jonathan Manera <manerajona@gmail.com>
2022-10-17 08:47:08 -0300

    Liquibase Example

---
+++ src/test/java/com/github/manerajona/MyAppTest.java
@@ -0,0 +1,12 @@
+package com.github.manerajona;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyAppTest {
+
+    @Test
+    public void dummy() {
+        Assert.assertEquals(1, 1);
+    }
+}
